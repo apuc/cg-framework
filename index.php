a0fcbbbcaa0f8f39c0161160d9f78a1c8cee1246
kavalar <apuc06@mail.ru>
2020-02-06 18:17:13 +0300

    fix console

--- index.php
+++ index.php
@@ -5,6 +5,4 @@ ini_set('display_errors', 1);
 
 require __DIR__ . '/vendor/autoload.php';
 
-var_dump( parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));die();
-
 \core\App::start()->setConfig()->setRouting()->run();
