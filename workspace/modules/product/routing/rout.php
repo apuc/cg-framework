16c9962c99df7dffa55362df0f3cda46c0c0b87c
king199025 <korol_dima@list.ru>
2020-05-29 18:24:46 +0300

    add test catalog and test order

--- workspace/modules/product/routing/rout.php
+++ workspace/modules/product/routing/rout.php
@@ -3,4 +3,6 @@ use core\App;
 
 App::$collector->get('product/download', ['workspace\modules\product\controllers\ProductController','actionDownload']);
 App::$collector->gridView('product', ['workspace\modules\product\controllers\ProductController']);
+App::$collector->get('catalog', ['workspace\modules\product\controllers\TestFrontController', 'actionCatalog']);
+App::$collector->any('testfront/order/{id}', ['workspace\modules\product\controllers\TestFrontController', 'actionOrder']);
 //App::$collector->get('product',['workspace\modules\product\controllers\ProductController','actionIndex']);
--- workspace/modules/product/services/ProductXML.php
+++ workspace/modules/product/services/ProductXML.php
@@ -3,6 +3,7 @@ namespace workspace\modules\product\services;
 
 use core\Debug;
 use workspace\modules\product\models\Product;
+use workspace\modules\product\models\ProductPhoto;
 use workspace\modules\product\models\VirtualProduct;
 
 class ProductXML
@@ -12,6 +13,12 @@ class ProductXML
    public function executeXML($path = 'product.xml'){
        $this->xml = simplexml_load_file($path);
        foreach ($this->xml->product as $prod){
+           foreach ($prod->images->image as $img){
+               $photo = new ProductPhoto();
+               $photo->product_id = (int)$prod->attributes()->id;
+               $photo->photo =(string)$img->attributes()->src;
+               $photo->save();
+           }
            if(Product::where('id',(int)$prod->attributes()->id)->first()) continue;
            $product = new Product();
            $product->id = (int)$prod->attributes()->id;
@@ -21,6 +28,7 @@ class ProductXML
            $virtual_product = new VirtualProduct();
            $virtual_product->product_id = (int)$prod->attributes()->id;
            $virtual_product->price = (float)$prod->price;
+           $photo->save();
            $product->save();
            $virtual_product->save();
        }
