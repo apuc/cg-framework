8b7b6db1cc5334b655644494ec4fc3244d5c82f8
double-null <double-null@yandex.ua>
2020-12-09 17:39:11 +0300

    Change load Smarty

--- init.php
+++ init.php
@@ -5,6 +5,3 @@ define("ROUTING_DIR", __DIR__ . '/routing');
 define("WORKSPACE_DIR", __DIR__ . '/workspace');
 define("RESOURCES_DIR", __DIR__ . '/resources');
 define("CORE_DIR", __DIR__ . '/core');
-
-require_once __DIR__ . '/core/smarty/Autoloader.php';
-include __DIR__ . '/core/smarty/bootstrap.php';
