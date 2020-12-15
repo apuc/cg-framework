97eeab47c28ec1c384d2c07b0a15b70c83a4bb81
Koronich <aleksadnrkorona@gmail.com>
2020-12-15 13:03:51 +0300

    class Tags created

---
+++ workspace/modules/tags/Tags.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace workspace\modules\tags;
+
+
+use core\App;
+
+class Tags
+{
+    public static function run()
+    {
+        $config['adminLeftMenu'] = [
+            [
+                'title' => 'Теги',
+                'url' => '/admin/tags',
+                'icon' => '<i class="nav-icon fa fa-users"></i>', //TODO
+            ],
+        ];
+
+        App::mergeConfig($config);
+    }
+}
