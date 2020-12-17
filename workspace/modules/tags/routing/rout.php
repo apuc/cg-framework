65c43dcc5003f9277d549f21a376523eaa4bd9e7
Koronich <aleksadnrkorona@gmail.com>
2020-12-17 15:47:33 +0300

    Changed routing adding method to gridView

--- workspace/modules/tags/routing/rout.php
+++ workspace/modules/tags/routing/rout.php
@@ -2,4 +2,4 @@
 
 use core\App;
 
-App::$collector->crud('tags', ['workspace\modules\tags\controllers\TagsController']);
+App::$collector->gridView('tags', ['workspace\modules\tags\controllers\TagsController']);
