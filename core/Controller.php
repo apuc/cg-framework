fd88082a42f1514dc3c2c304c5d3092fcdfb945c
Koronich <aleksadnrkorona@gmail.com>
2020-12-17 15:39:22 +0300

    Added SERVER_PORT in redirect()

--- core/Controller.php
+++ core/Controller.php
@@ -56,7 +56,7 @@ class Controller
         $uri = sprintf(
             "%s://%s",
             isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
-            $_SERVER['SERVER_NAME']
+            $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT']
         );
 
         header('Location: ' . $uri . '/' . $url);
